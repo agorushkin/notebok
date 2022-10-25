6cfa411d86eb2d2a58dd126e7bf7841410ea55e7
Arsenii Gorushkin <arsenii.gorushkin@gmail.com>
2022-10-25 23:36:17 +1300

    fix: fixed secure protocol connection

--- client/scripts/socket.js
+++ client/scripts/socket.js
@@ -1,4 +1,5 @@
-const socket = new WebSocket(`ws://${ window.location.host }/ws/${ pageUUID }`);
+const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+const socket = new WebSocket(`${ protocol }://${ window.location.host }/ws/${ pageUUID }`);
 
 socket.onmessage = ({ data }) => {
   contentField.value = data;
